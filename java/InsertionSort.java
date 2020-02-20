59cf4c4734f89aa0c90aa1a910a7c43aa7fd164a
Felix Ståhl <31844847+felixstahl@users.noreply.github.com>
2020-02-20 20:26:42 +0100

    Update InsertionSort.java

--- java/InsertionSort.java
+++ java/InsertionSort.java
@@ -5,10 +5,11 @@ public class InsertionSort{
     for (int j = 1; j < n; j++){
       int key = array[j];
       int i = j - 1;
-      while ((i > -1) && (array[i] > key)){
+      while((i > -1) && (array[i] > key)){
         array [i+1] = array [i];
         i--;
-      } array[i+1] = key;
+      } 
+      array[i+1] = key;
     }
   }
 
